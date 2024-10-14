44574bb96244d1c50f74bcfe5005c8c41a7c6741
Thomas Gosse <thms.gosse@gmail.com>
2024-10-14 20:15:52 +0200

    feat: add dijkstra algorithm and tests

--- src/graphs/dijkstra.test.ts
+++ src/graphs/dijkstra.test.ts
@@ -1,21 +1,188 @@
-import { test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { dijkstra } from "./dijkstra";
 
-test("dijkstra", () => {
-  const graph = {
-    Start: {
-      A: 6,
-      B: 2,
-    },
-    A: {
-      End: 1,
-    },
-    B: {
-      A: 3,
-      End: 5,
-    },
-    End: {},
-  };
+describe("dijkstra", () => {
+  test("example 1", () => {
+    const graph = {
+      Start: {
+        A: 6,
+        B: 2,
+      },
+      A: {
+        End: 1,
+      },
+      B: {
+        A: 3,
+        End: 5,
+      },
+      End: {},
+    };
 
-  dijkstra("Start", "End", graph);
+    /**
+     * Init
+     * costs { A: 6, B: 2, End: undefined }
+     * parents { A: Start, B: Start, End: undefined }
+     * processed <empty>
+     *
+     * Round 1
+     * closest not in processed is B
+     * update A: from B (3) + B from start (2) = 5
+     * update End: from B (5) + B from start (2) = 7
+     * costs { A: 5, B: 2, End: 7 }
+     * parents { A: B, B: Start, End: B }
+     * processed <B>
+     *
+     * Round 2
+     * closest not in processed is A
+     * update End because a cheaper path is found: from A (1) + cost to reach A (5) = 6
+     * costs { A: 5, B: 2, End: 6 }
+     * parents { A: B, B: Start, End: A }
+     * processed <A,B>
+     *
+     * Result
+     * Start -> B -> A -> End for 6
+     */
+
+    expect(dijkstra("Start", "End", graph)).toStrictEqual(["Start", "B", "A", "End"]);
+  });
+
+  test("example 2", () => {
+    const graph = {
+      Book: {
+        Disk: 5,
+        Poster: 0,
+      },
+      Disk: {
+        Guitar: 15,
+        Battery: 20,
+      },
+      Poster: {
+        Guitar: 30,
+        Battery: 35,
+      },
+      Guitar: {
+        Piano: 20,
+      },
+      Battery: {
+        Piano: 10,
+      },
+      Piano: {},
+    };
+
+    expect(dijkstra("Book", "Piano", graph)).toStrictEqual(["Book", "Disk", "Battery", "Piano"]);
+  });
+
+  test("graph with cycle", () => {
+    const graph = {
+      A: {
+        B: 1,
+        C: 4,
+      },
+      B: {
+        C: 2,
+        D: 5,
+      },
+      C: {
+        A: 3,
+        D: 1,
+      },
+      D: {},
+    };
+
+    expect(dijkstra("A", "D", graph)).toStrictEqual(["A", "B", "C", "D"]);
+  });
+
+  test("multiple shortest paths", () => {
+    const graph = {
+      A: {
+        B: 1,
+        C: 2,
+      },
+      B: {
+        End: 2,
+      },
+      C: {
+        End: 1,
+      },
+      End: {},
+    };
+
+    // There are two paths: A -> B -> End (cost 3) and A -> C -> End (cost 3)
+    try {
+      expect(dijkstra("A", "End", graph)).toStrictEqual(["A", "C", "End"]);
+    } catch {
+      expect(dijkstra("A", "End", graph)).toStrictEqual(["A", "B", "End"]);
+    }
+  });
+
+  test("disconnected nodes", () => {
+    const graph = {
+      A: {
+        B: 2,
+      },
+      B: {
+        C: 3,
+      },
+      C: {},
+      D: {
+        E: 1,
+      },
+      E: {},
+    };
+
+    expect(dijkstra("A", "D", graph)).toStrictEqual([]);
+  });
+
+  test("start and end same node", () => {
+    const graph = {
+      A: {
+        B: 2,
+      },
+      B: {
+        C: 3,
+      },
+      C: {},
+    };
+    expect(dijkstra("A", "A", graph)).toStrictEqual(["A"]);
+  });
+
+  test("large graph", () => {
+    const graph = {
+      Start: {
+        A: 10,
+        B: 5,
+      },
+      A: {
+        C: 1,
+        D: 2,
+      },
+      B: {
+        A: 3,
+        D: 9,
+        E: 2,
+      },
+      C: {
+        D: 4,
+        End: 2,
+      },
+      D: {
+        End: 6,
+      },
+      E: {
+        D: 2,
+        End: 4,
+      },
+      End: {},
+    };
+    expect(dijkstra("Start", "End", graph)).toStrictEqual(["Start", "B", "E", "End"]);
+  });
+
+  test("graph with no connections", () => {
+    const graph = {
+      A: {},
+      B: {},
+    };
+
+    expect(dijkstra("A", "B", graph)).toStrictEqual([]);
+  });
 });
--- src/graphs/dijkstra.ts
+++ src/graphs/dijkstra.ts
@@ -1,3 +1,51 @@
-export type WeightedGraph = Record<string, Record<string, number>>;
+import { construct_shortest_path } from "./construct-shortest-path.js";
+import { WeightedGraph } from "./types.js";
 
-export function dijkstra(start: string, target: string, graph: WeightedGraph) {}
+function init_parents_costs(start: string, target: string, graph: WeightedGraph) {
+  let parents: Record<string, string> = { [target]: undefined };
+  let costs: Record<string, number> = { [start]: 0, [target]: Infinity };
+  for (let child of Object.keys(graph[start])) {
+    parents[child] = start;
+    costs[child] = graph[start][child];
+  }
+  return { parents, costs };
+}
+
+function find_closest_child(children: Record<string, number>, processed: Set<string>) {
+  let lowest_cost = Infinity;
+  let closest_node = undefined;
+  for (let child of Object.keys(children)) {
+    if (!processed.has(child) && children[child] < lowest_cost) {
+      lowest_cost = children[child];
+      closest_node = child;
+    }
+  }
+  return closest_node;
+}
+
+export function dijkstra(start: string, target: string, graph: WeightedGraph) {
+  const processed = new Set<string>();
+  let { parents, costs } = init_parents_costs(start, target, graph);
+  let current = find_closest_child(costs, processed);
+
+  while (current) {
+    const children = graph[current];
+    for (let child of Object.keys(children)) {
+      // It means we did not encountered this child yet
+      if (typeof costs[child] === "undefined") {
+        costs[child] = Infinity;
+      }
+      const cost_to_child = graph[current][child] + costs[current];
+      if (cost_to_child < costs[child]) {
+        costs[child] = cost_to_child;
+        parents[child] = current;
+      }
+    }
+    processed.add(current);
+    current = find_closest_child(costs, processed);
+  }
+
+  // This condition handles disconnected nodes graphs
+  if (costs[target] === Infinity && start !== target) return [];
+  return construct_shortest_path(start, target, parents);
+}
