bf12f8c965c911f4ccf218a616d92ff05c4cede2
Thomas Gosse <thms.gosse@gmail.com>
2024-11-14 20:38:47 +0100

    refactor: rename cache into unique_subsets

--- src/structures/power-set.ts
+++ src/structures/power-set.ts
@@ -3,22 +3,22 @@ function hash(set: Set<string>) {
 }
 
 // @todo: clean and write tests
-export function create_power_set(set: Set<string>, cache: Record<string, Set<string>> = {}): Set<Set<string>> {
+export function create_power_set(set: Set<string>, unique_subsets: Record<string, Set<string>> = {}): Set<Set<string>> {
   if (set.size === 0) {
     return new Set([set]);
   }
 
-  cache[hash(set)] = set;
+  unique_subsets[hash(set)] = set;
 
   set.keys().forEach((element) => {
     const subset = new Set(set);
     subset.delete(element);
-    cache[hash(subset)] = set;
+    unique_subsets[hash(subset)] = set;
 
-    create_power_set(subset, cache).forEach((subset) => {
-      cache[hash(subset)] = subset;
+    create_power_set(subset, unique_subsets).forEach((subset) => {
+      unique_subsets[hash(subset)] = subset;
     });
   });
 
-  return new Set(Object.values(cache));
+  return new Set(Object.values(unique_subsets));
 }
